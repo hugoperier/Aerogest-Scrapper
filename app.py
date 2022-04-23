c64280589c0c534e3f4cccc9efd326fc349fb31e
hugo <hugo.perier@cern.ch>
2022-04-23 17:12:15 +0200

    Refacto scrapper into a class made for daily booking planning

--- app.py
+++ app.py
@@ -1,58 +1,15 @@
-import requests
-from bs4 import BeautifulSoup
-from src.scrapper import extract_instructor_resas
-import re
-import cssutils
+from src.scrapper import DailyScrapper
+import configparser
 
 def main():
-    BASE_URI = "aerogest-reservation.com"
-    CLUB_ID = "ac/bellegarde"
-    AEROCLUB = "aeroclub_bellegarde"
-    URI = "http://" + BASE_URI + "/" + CLUB_ID + ".htm"
-    LOGIN = ""
-    PASSWORD = ""
-    AVAILABLE_COLOR = "#B8F3A8"
-    TOTAL_MINUTES = 1020
-    START_HOUR = 6
+    config = getConfig()
+    scrapper = DailyScrapper(config)
+    scrapper.extract()
 
-    LOGIN_URI = "https://" + BASE_URI + "/Connection/logon"
-    BOOKING_URI = "https://" + BASE_URI + "/Booking/PlanningInstructor"
-    DAILY_URI = "https://" + BASE_URI + "/Booking/PlanningDateChange?askedDate=mardi+19%2F04%2F2022&button=mc"
-
-    session = requests.Session()
-    session.post(LOGIN_URI, data={
-        "login": LOGIN,
-        "pass": PASSWORD,
-        "aeroclub": AEROCLUB,
-        "action2": "Connexion"
-    })
-    session.cookies.set("hasConsent", "true")
-    session.cookies.set("Aerogest-reservation-str", AEROCLUB)
-
-    req_daily = session.get(DAILY_URI)
-    soup = BeautifulSoup(req_daily.text, "html.parser")
-    rows = soup.find_all("td", {"class": "referenceContentTD"})
-    #print(rows)
-    for row in rows:
-        #available_area = row.select_one('div[style*="#B8F3A8"]')
-        available_area = row.find('div', style=lambda value: value and 'background-color: #B8F3A8' in value)
-        if (available_area):
-            print(available_area)
-            style = cssutils.parseStyle(available_area.get('style'))
-            width = float((style['width']).replace("%", ""))
-            left = float(style['left'].replace("%", ""))
-
-            start = (round(left / 100 * TOTAL_MINUTES) / 60) + START_HOUR
-            end = (round(width / 100 * TOTAL_MINUTES) / 60) + start
-            print(start, end)
-
-        else:
-            print("nothing")
-
-    # req_booking = session.get(BOOKING_URI)
-    # tooltips = extract_instructor_resas(req_booking.text)
-    # for tooltip in tooltips:
-    #     print(tooltip)
+def getConfig():
+    config = configparser.ConfigParser()
+    config.read('default.conf')
+    return config
 
 if __name__ == "__main__":
     main()
--- src/scrapper.py
+++ src/scrapper.py
@@ -1,59 +1,96 @@
 from bs4 import BeautifulSoup
 import locale
+import requests
+import re
+import cssutils
+import datetime
 
-def extract_tooltip(dt):
-    dt_soup = BeautifulSoup(dt, "html.parser")
-    infos = dt_soup.find_all("td")
-    resa = {}
-    while infos:
-        if infos[0].text.endswith(":"):
-            key = infos[0].text.strip(":")
-            resa[key] = []
-        else:
-            if (infos[0].text != ""):
-                resa[key].append(infos[0].text.strip())
-        infos = infos[1:]
-    for key in resa:
-        if (resa[key].__len__() == 1):
-            resa[key] = resa[key][0]
-    return resa
-
-def extract_instructor_resas(html):
-    soup = BeautifulSoup(html, "html.parser")
-    rows = soup.find_all("td", {"class": "referenceContentTD"})
-    tooltips = []
-    for row in rows:
-        resa = row.find("div", {"class": "reservation"})
-        if (resa):
-            dt = resa["data-tooltip"]
-            tooltip = extract_tooltip(dt)
-            tooltips.append(tooltip)
-    return tooltips
-
-class AerogestScrapper:
+class DailyScrapper:
     def __init__(self, config):
+        locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
         self.config = config
         self.session = requests.Session()
+        self.available_color = "#B8F3A8"
+        self.total_minutes = 1020
+        self.start_hour = 6
+        self.date = datetime.datetime.now()
         self.login()
-        locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
 
     def login(self):
-        self.session.post(self.config["login_uri"], data={
-            "login": self.config["login"],
-            "pass": self.config["password"],
-            "aeroclub": self.config["aeroclub"],
+        self.session.post(f'{self.config["AEROGEST_INFOS"]["HOST"]}/Connection/logon', data={
+            "login": self.config["CREDENTIALS"]["LOGIN"],
+            "pass": self.config["CREDENTIALS"]["PASSWORD"],
+            "aeroclub": self.config["AEROGEST_INFOS"]["CLUB_ID"],
             "action2": "Connexion"
         })
         self.session.cookies.set("hasConsent", "true")
-        self.session.cookies.set("Aerogest-reservation-str", self.config["aeroclub"])
+        self.session.cookies.set("Aerogest-reservation-str", self.config["AEROGEST_INFOS"]["CLUB_ID"])
 
-    def getScheduleByDate(self, date):
-        dateFormatted = date.strftime("%A+%d %m %Y&button=mc").replace(" ", "%2F")        
-        daily_uri = self.config["daily_uri"] + "?askedDate=" + dateFormatted
-        req_daily = self.session.get(daily_uri)
-        availableAreas = self.getAvailableAreas(req_daily.text)
-        reservedAreas = self.getReservedAreas(req_daily.text)
+    def extract(self):
+        dateFormatted = self.date.strftime("%A+%d %m %Y&button=mc").replace(" ", "%2F")        
+        req_daily = self.session.get(f'{self.config["AEROGEST_INFOS"]["HOST"]}/Booking/PlanningDateChange?askedDate={dateFormatted}')
+        airplane_availability = self.getAirplaneAvailability(req_daily.text)
+        instructors_availability = self.getInstructorsAvailability(airplane_availability)
 
+
+    def getAirplaneAvailability(self, html):
+        timeblockTable = BeautifulSoup(html, "html.parser").find_all("table", {"class": "timeblockTable"})[1]
+
+        rows = timeblockTable.find_all("tr")
+        for row in rows:
+            referenceHeader = row.find("td", {"class": "referenceHeader"})
+            if not referenceHeader:
+                continue
+            header = getHeader(referenceHeader)
+            availability = getAvailability(row)
+            schedules = getSchedules(row)
+
+    def getHeader(self, referenceHeader):
+        dt = referenceHeader["data-tooltip"].text
+        dtLines = dt.split("<br>")
+        header = {}
+        for line in dtLines:
+            [key, value] = line.split(":")
+            header[key] = value.strip()
+        return header
+
+    def getReservations(self, row):
+        referenceContentTD = row.find_all("td", {"class": "referenceContentTD"})
+        reservations = row.find_all("div", {"class": "reservation"})
+        for reservation in reservations:
+            dt = reservation["data-tooltip"]
+            reservationParsed = self.extract_tooltip(dt)
+
+    def extract_tooltip(self, dt):
+        dt_soup = BeautifulSoup(dt, "html.parser")
+        infos = dt_soup.find_all("td")
+        resa = {}
+        while infos:
+            if infos[0].text.endswith(":"):
+                key = infos[0].text.strip(":")
+                resa[key] = []
+            else:
+                if (infos[0].text != ""):
+                    resa[key].append(infos[0].text.strip())
+            infos = infos[1:]
+        for key in resa:
+            if (resa[key].__len__() == 1):
+                resa[key] = resa[key][0]
+        return resa
+
+    def getAvailability(self, row):
+        referenceContentTD = row.find_all("td", {"class": "referenceContentTD"})
+        greenZones = row.find_all('div', style=lambda value: value and 'background-color: #B8F3A8' in value)
+        availability = []
+        for greenZone in greenZones:
+            style = cssutils.parseStyle(greenZone.get('style'))
+            width = float((style['width']).replace("%", ""))
+            left = float(style['left'].replace("%", ""))
+            schedule = {}
+            schedule.start = (round(left / 100 * TOTAL_MINUTES) / 60) + START_HOUR
+            schedule.end = (round(width / 100 * TOTAL_MINUTES) / 60) + schedule.start
+            availability.append(schedule)
+        return availability
 
 
     def getAvailableAreas(self, content):
@@ -61,7 +98,6 @@ class AerogestScrapper:
         rows = soup.find_all("td", {"class": "referenceContentTD"})
         #print(rows)
         for row in rows:
-            #available_area = row.select_one('div[style*="#B8F3A8"]')
             available_area = row.find('div', style=lambda value: value and 'background-color: #B8F3A8' in value)
             if (available_area):
                 print(available_area)
